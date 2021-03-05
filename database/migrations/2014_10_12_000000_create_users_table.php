a4e4c23e3dbb7eb2ec3af73bfde70b23589a66b5
Thom711 <tvdveldt@hotmail.com>
2021-03-05 14:25:09 +0100

    added migrations, model relations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('address');
+            $table->boolean('checked');
+            $table->text('description');
+            $table->text('interests')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('email');
+            $table->integer('account');
+            $table->foreignId('creditcard_id')->contrained();
             $table->timestamps();
         });
     }
